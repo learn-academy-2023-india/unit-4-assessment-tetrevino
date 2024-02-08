3c6ec5381845576b928706ad3bab9bc1adb45b34
tetrevino <tomas_trevino@yahoo.com>
2024-02-08 11:33:58 -0800

    assessment 4

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -11,26 +11,60 @@
 // --------------------1) Prompt: Create a function that takes in a number (greater than 2) and returns an array containing the Fibonacci sequence. The length of the array is determined by the argument of the function.
 
 // a) Create a test with expect statements for each of the variables provided.
+// describe("fib", () => {
+//  it("takes in a number (greater than 2) and returns an array containing the Fibonacci sequnce", () => {
+//     const fibonacciLength1 = 6
+//     expect(fib(fibonacciLength1)).toEqual([1, 1, 2, 3, 5, 8])
 
-const fibonacciLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+//     const fibonacciLength2 = 10
+//     expect(fib(fibonacciLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+// // Expected output: 
+   
+    
+ 
+// })})
 
-const fibonacciLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+// // b) Create the function that makes the test pass.
 
-// b) Create the function that makes the test pass.
+// const fib = (num) => {
+//     const sequence = [1,1]
 
-// Pseudo code:
+//     for(i=2; i < num; i++) {
+//         const nextNum = sequence[i-1] + sequence[i-2]
+//         sequence.push(nextNum)
+//     }
+//     return sequence
+// }
+
+// Pseudo code: I had to AI this one, and view other code, but it makes sense in hindsight. Any two numbers add togther to create the next iteration in the sequence. We start the iteration at the [2] index and then add the first two numbers to give us 3. That value is then appeneded to the end of the array sequence. The number we pass is the length of the array at the end. 
+
+
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
 // a) Create a test with expect statements for each of the variables provided.
+// describe("calculateVotes", () => {
+//     it("calculates the difference between upvotes and downvotes", () => { 
+//     const votes1 = { upVotes: 13, downVotes: 2 }
+//     // Expected output: 11
+//     expect(calculateVotes(votes1)).toEqual(11)
 
-const votes1 = { upVotes: 13, downVotes: 2 }
-// Expected output: 11
-const votes2 = { upVotes: 2, downVotes: 33 }
-// Expected output: -31
+//     const votes2 = { upVotes: 2, downVotes: 33 }
+//     // Expected output: -31
+//     expect(calculateVotes(votes2)).toEqual(-31)
+//     })
+// })
 
-// b) Create the function that makes the test pass.
 
-// Pseudo code:
+
+
+// // b) Create the function that makes the test pass.
+
+// const  calculateVotes = (votes) => {
+    
+//         return votes.upVotes - votes.downVotes
+   
+      
+// }
+// // Pseudo code: The test calls for a difference in votes, particularly, upvotes to downvotes. I figured an if statement that added votes, then an else statement which removed votes. But blackbox and others said just to remove a vote from the upvote to simulate the difference.
+
--- interview-questions.md
+++ interview-questions.md
@@ -10,29 +10,29 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Similarities
 
-1.
-2.
-3.
+1.JavaScript and Ruby are both oop languages.
+2.Both allocate thier own garbage.
+3.Both are used in web-development.
 
 Differences
 
-1.
-2.
-3.
+1.Everything in Ruby is an object, not in JavaScript.
+2.JavaScript has better use cases on the front-end, while Ruby on the backend.
+3.Ruby has simpler syntax for beginners, unlike JavaScript.
 
-Researched answer:
+Researched answer: JS and RUby are both dynamic, high-level lanaguegs, and are object-oritented; but have difference use cases, syntax, memory management, and development focus.
 
 2. What does it mean to implement a TDD workflow?
 
-Your answer:
+Your answer: Implements testing on your code, as you go, to ensure code is functional everstep of the way. 
 
-Researched answer:
+Researched answer: A testing methodology in which tests are written prior to code, red to green, fail then build code to pass.
 
 3. What is a block in Ruby?
 
-Your answer:
+Your answer: Portion of the code to execute when needed.
 
-Researched answer:
+Researched answer: Block serves as parameters to methods that take blocks. It can be passed into functions or method calls, in Ruby.
 
 4. STRETCH: What is a version manager?
 
@@ -42,8 +42,8 @@ Answer:
 
 Research and define the following terms.
 
-1. Database migrations:
+1. Database migrations: Anytime you make changes to your database, a migration is created to update the schema and implement the change to your database. 
 
-2. Model validations:
+2. Model validations: Validations check if your attributes are correct, based on a series of criteria like length, or format. 
 
-3. Params:
+3. Params: Params are the attributes you pass to the controller, in your rails application, when a request is made. 
